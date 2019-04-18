0ce06f408d053285dfb91fe9cbdcb184742a0c57
milana.campbell <milana.campbell@gmail.com>
2019-04-18 15:52:52 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        int secretNumber, guess = 0;//guess is initialized to 0
+
+
+        secretNumber = 123;
+
+        System.out.println("I'm thinking of a number between 1 and 1000");
+        System.out.print("Enter the number:");
+        guess = keyboard.nextInt();
+
+
+        while (guess != secretNumber) {
+            //do your thing as long as the expression is true
+
+            System.out.println("\nYou are wrong. Try again.");
+            System.out.println("Enter the number:");
+            guess = keyboard.nextInt();
+        }
+        System.out.println("You are correct. You win a prize!");
+        keyboard.close();
+    }
+
+}
+
+
+
+
+
